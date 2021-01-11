95cb3e89d5efb1f71f0b76764a7b37f05eb4db4a
zbiedinger <zbiedinger@mac.com>
2021-01-11 16:41:51 -0500

    added delete dropdown menu

--- Client/app.js
+++ Client/app.js
@@ -17,6 +17,16 @@ $(function(){
     });
     })
 })
+
+$(function(){
+    $.get("https://localhost:44325/api/movie", function(data){
+        
+        //$('#response pre').html( JSON.stringify(data) );
+    data.map(element => {
+        $('#currentMoviesDelete').append(`<li><a onClick="confirmDelete(${element.movieId})">${element.title}</a></li> `)
+    });
+    })
+})
 //<li><a href="#">HTML</a></li>
 
 function generateUpdateForm(currentMovieID){
@@ -51,6 +61,16 @@ function updateMovie(currentMovie){
             console.log( errorThrown );
         }
     });
+}
+
+function confirmDelete(currentMovie){
+    $.get("https://localhost:44325/api/movie/" + currentMovieID, function(data){
+
+    $('#updateMovie').append(`<div class="form-group">
+    <input type="text" class="form-control" id="title" name="title" value="${data.title}">
+</div>
+<button type="submit" class="btn btn-purple-light">Confirm</button>`)
+    })
 }
 
 
