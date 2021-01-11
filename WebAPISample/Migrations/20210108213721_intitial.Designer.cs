e85a88fd4425c8698cdbe528ee4e8b6d92e2bdd0
zbiedinger <zbiedinger@mac.com>
2021-01-10 23:36:56 -0500

    Made pleasing aesthetic for webpage

--- WebAPISample/Migrations/20210108213721_intitial.Designer.cs
+++ WebAPISample/Migrations/20210108213721_intitial.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPISample.Data;
 
 namespace WebAPISample.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210108213721_intitial")]
+    partial class intitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
