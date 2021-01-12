06654ab23bf1dbe0b7c06d0f47b5cdd170c4f15a
zbiedinger <zbiedinger@mac.com>
2021-01-12 13:26:29 -0500

    made progress on updating a movie in the database

--- Client/app.js
+++ Client/app.js
@@ -1,7 +1,6 @@
 $(function(){
     $.get("https://localhost:44325/api/movie", function(data){
         
-        //$('#response pre').html( JSON.stringify(data) );
     data.map(element => {
         $('#response pre').append(`<div><div>Title: ${element.title} Director: ${element.director} Genre: ${element.genre}</div></div`)
     });
@@ -11,7 +10,6 @@ $(function(){
 $(function(){
     $.get("https://localhost:44325/api/movie", function(data){
         
-        //$('#response pre').html( JSON.stringify(data) );
     data.map(element => {
         $('#currentMovies').append(`<li><a onClick="generateUpdateForm(${element.movieId})">${element.title}</a></li> `)
     });
@@ -21,19 +19,19 @@ $(function(){
 $(function(){
     $.get("https://localhost:44325/api/movie", function(data){
         
-        //$('#response pre').html( JSON.stringify(data) );
     data.map(element => {
         $('#currentMoviesDelete').append(`<li><a onClick="confirmDelete(${element.movieId})">${element.title}</a></li> `)
     });
     })
 })
-//<li><a href="#">HTML</a></li>
 
 function generateUpdateForm(currentMovieID){
     
     $.get("https://localhost:44325/api/movie/" + currentMovieID, function(data){
 
     $('#updateMovie').append(`<div class="form-group">
+    <input type="number" class="form-control" id="movieID" name="movieId" value="${data.movieId}">
+
     <input type="text" class="form-control" id="title" name="title" value="${data.title}">
 </div>
 <div class="form-group">
@@ -47,46 +45,85 @@ function generateUpdateForm(currentMovieID){
     })
 }
 
-function updateMovie(currentMovie){
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        dataType: 'json',
-        type: 'put',
-        contentType: 'application/json',
-        data: JSON.stringify(dict),
-        success: function( data, textStatus, jQxhr ){
-            $('#response pre').html( data );
-        },
-        error: function( jqXhr, textStatus, errorThrown ){
-            console.log( errorThrown );
-        }
+(function(){
+    function update_Movie(currentMovie){
+        var dict = {
+            MovieId : this["movieId"].value,
+        	Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        //dict.MovieID.parseInt(MovieId);
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.dict,
+            success: function (data, textStatus, jQxhr) {
+                $('#updateMovie').html(`<br>
+                <div class="alert alert-purple" role="alert">
+                    Thanks for updating ${data.title} to the best list of movies on this side of the Mississippi!
+                </div>`);            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+    }
+
+    $('#updateMovie').on('submit', function(e){
+        e.preventDefault();
     });
-}
+
+
+    $('#updateMovie').submit(update_Movie);
+})(jQuery);
 
 function confirmDelete(currentMovieID){
     $.get("https://localhost:44325/api/movie/" + currentMovieID, function(data){
 
-        $('#updateMovieDelete').append(`
+        $('#deleteMovie').append(`
         <h4>Do you want to perminatly delete: <strong>${data.title}</strong></h4>
-        <button onClick="DeleteMovie(${data.movieId})" class="btn btn-purple-light">Confirm</button>`)
+        <input type="text" class="form-control" id="currentMovieID" name="genre" value="${data.movieId}">
+        <button type="submit" class="btn btn-purple-light">Confirm</button>`)
     })
 }
 
-function DeleteMovie(deletedMovieId) {
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        dataType: 'json',
-        type: 'delete',
-        contentType: 'application/json',
-        data: JSON.stringify(dict),
-        success: function( data, textStatus, jQxhr ){
-            $('#response pre').html( data );
-        },
-        error: function( jqXhr, textStatus, errorThrown ){
-            console.log( errorThrown );
-        }
+(function ($) {
+    function delete_Movie(deletedMovieId) {
+        $.ajax({
+            url: 'https://localhost:44325/api/movie/' + deletedMovieId,
+            dataType: 'json',
+            type: 'delete',
+            contentType: 'application/json',
+            success: function (data, textStatus, jQxhr) {
+                $('#deleteMovie').html(`<br>
+                    <div class="alert alert-purple" role="alert">
+                        Thanks for deleting ${data.title} movie!
+                    </div>`);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+
+        //deletedMovieId.preventDefault();
+    }
+
+    $('#deleteMovie').on('submit', function(e){
+        e.preventDefault();
     });
-}
+
+
+    $('#deleteMovie').submit(delete_Movie);
+
+
+    
+
+})(jQuery);
+
 
 (function($){
     function processForm( e ){
